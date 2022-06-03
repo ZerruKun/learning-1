d33e2d4eb0c886f6f5106e524c025f50223c883c
Tabalin Nikolay <Zerru_Kun@yahoo.com>
2022-06-03 16:40:20 +0300

    Сортировка.

--- src/App.js
+++ src/App.js
@@ -4,35 +4,55 @@ import PostForm from "./components/PostForm";
 //import Counter from "./components/Counter";
 //import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
+import MySelect from "./components/UI/select/MySelect";
 import "./styles/App.css";
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: "a", body: "б" },
+    { id: 2, title: "г", body: "а" },
+    { id: 3, title: "я", body: "в" },
+  ]);
+
+  const [selectedSort, setSelectedSort] = useState("");
+
   //Добавление поста
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
-  }
+  };
   //Удаление поста
   const removePost = (post) => {
-    setPosts(posts.filter(p => p.id !== post.id));
-  }
+    setPosts(posts.filter((p) => p.id !== post.id));
+  };
 
-  const [posts, setPosts] = useState([
-    {id:1, title:"JavaScritp 1", body:"Description"},
-    {id:2, title:"JavaScritp 2", body:"Description"},
-    {id:3, title:"JavaScritp 3", body:"Description"}
-  ]);
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+    setPosts([...posts].sort((a,b) => a[sort].localeCompare(b[sort]))) //sort мутирует массив, поэтому копия.
+  }
 
   return (
     <div className="App">
-      <PostForm create={createPost}/>
+      <PostForm create={createPost} />
+      <hr style={{ margin: "15px 0" }}></hr>
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue="Сортировка"
+          options={[
+            { value: "title", name: "По названию" },
+            { value: "body", name: "По описанию" },
+          ]}
+        />
+      </div>
       {/* Условная отрисовка */}
-      {posts.length !== 0 
-        ? <PostList remove={removePost} posts={posts} title="Посты про JS"/>
-        : <h1 style={{textAlign : "center"}}>Посты не были найдены!</h1>
-      }
+      {posts.length !== 0 ? (
+        <PostList remove={removePost} posts={posts} title="Посты про JS" />
+      ) : (
+        <h1 style={{ textAlign: "center" }}>Посты не были найдены!</h1>
+      )}
     </div>
   );
-
 }
 
 export default App;
