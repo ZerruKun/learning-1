e3058350eea0a224e1ccb46582ca0fe783f0524b
Tabalin Nikolay <Zerru_Kun@yahoo.com>
2022-06-03 12:42:56 +0300

    Исправление кнопки, декомпозиция формы.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
+import PostForm from "./components/PostForm";
 //import ClassCounter from "./components/ClassCounter";
 //import Counter from "./components/Counter";
 //import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
 import "./styles/App.css";
 
 function App() {
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+  }
 
   const [posts, setPosts] = useState([
     {id:1, title:"JavaScritp 1", body:"Description"},
@@ -15,30 +18,9 @@ function App() {
     {id:3, title:"JavaScritp 3", body:"Description"}
   ]);
 
-  const [post, setPost] = useState({title:"", body:""});
-
-  const AddNewPost = (e) => {
-    e.preventDefault();
-    setPosts([...posts, {...post, id: Date.now()}]);
-    setPost({title:"", body:""})
-  }
-
   return (
     <div className="App">
-      <form>
-        <MyInput 
-          type="text" 
-          placeholder="Название поста" 
-          value={post.title}
-          onChange={e => setPost({...post, title: e.target.value})}/>
-        <MyInput 
-          type="text" 
-          placeholder="Описание поста"
-          value={post.body} 
-          onChange={e => setPost({...post, body: e.target.value})}
-          />
-        <MyButton onClick={AddNewPost}>Создать пост</MyButton>
-      </form>
+      <PostForm create={createPost}/>
       <PostList posts={posts} title="Посты про JS"/>
     </div>
   );
--- src/components/UI/button/MyButton.jsx
+++ src/components/UI/button/MyButton.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import classes from "./MyButton.module.css"
 
-const MyButton = (props) => {
+const MyButton = ({children, ...props}) => {
   return (
     <button {...props} className={classes.myBtn}>
-        {props.children}
+        {children}
     </button>
   )
 }
