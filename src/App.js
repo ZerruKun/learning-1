98eca26ccbca418993b54c3e687260fc0282f388
Tabalin Nikolay <Zerru_Kun@yahoo.com>
2022-06-09 17:15:59 +0300

    Добавление навигации.

--- src/App.js
+++ src/App.js
@@ -1,91 +1,14 @@
-//import axios from "axios";
-import React, { useEffect, useState } from "react";
-import PostService from "./API/PostService";
-import PostFilter from "./components/PostFilter";
-import PostForm from "./components/PostForm";
-//import ClassCounter from "./components/ClassCounter";
-//import Counter from "./components/Counter";
-//import PostItem from "./components/PostItem";
-import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import Loader from "./components/UI/loader/Loader";
-import MyModal from "./components/UI/modal/MyModal";
-import Pagination from "./components/UI/pagination/Pagination";
-import { useFetching } from "./hooks/useFetching";
-import { usePosts } from "./hooks/usePosts";
-// import MyInput from "./components/UI/input/MyInput";
-// import MySelect from "./components/UI/select/MySelect";
-import "./styles/App.css";
-import { getPageCount, getPagesArray } from "./utils/pages";
+import React from "react";
+import AppRouter from "./components/AppRouter";
+import Navbar from "./components/UI/navbar/Navbar";
 
 function App() {
-  const [posts, setPosts] = useState([]);
-
-  // { id: 1, title: "а", body: "б" },
-  // { id: 2, title: "г", body: "а" },
-  // { id: 3, title: "я", body: "в" },
-
-  const [filter, setFilter] = useState({sort:"", query:""})
-
-  const [modal, setModal] = useState(false);
-
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-
-  const [totalPages, setTotalPages] = useState(0);
-
-  const[limit, setLimit] = useState(10);
-  const[page, setPage] = useState(1);
-
-  const [fetchPosts, isPostLoading, postError] = useFetching( async (limit, page) => {
-    const responce = await PostService.getAll(limit, page);
-    setPosts(responce.data);
-    const totalCount = responce.headers["x-total-count"];
-    setTotalPages(getPageCount(totalCount, limit));
-  })
-
-  useEffect(() => {
-    fetchPosts(limit, page)
-  }, [])
-
-  //Добавление поста
-  const createPost = (newPost) => {
-    setPosts([...posts, newPost]);
-    setModal(false);
-  };
-  //Удаление поста
-  const removePost = (post) => {
-    setPosts(posts.filter((p) => p.id !== post.id));
-  };
-
-  const changePage = (page) => {
-    setPage(page);
-    fetchPosts(limit, page);
-  }
-
   return (
-    <div className="App">
-      <button onClick={fetchPosts}>GET POSTS</button>
-      <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
-        Создать пост
-      </MyButton>
-      <MyModal visible={modal} setVisible={setModal}>
-        <PostForm create={createPost} />
-      </MyModal>
-      <hr style={{ margin: "15px 0" }}></hr>
-      <PostFilter
-        filter={filter} 
-        setFilter={setFilter} 
-      />
-      {postError && 
-        <h1>Произошла ошибка ${postError}</h1>
-      }
-      {isPostLoading 
-        ? <div style={{display: "flex", justifyContent: "center", marginTop: 50}}><Loader /></div>
-        : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS" />
-      }
-      <Pagination page={page} changePage={changePage} totalPages={totalPages}/>
+    <div>
+      <Navbar />
+      <AppRouter />
     </div>
-  );
+  )
 }
 
 export default App;
