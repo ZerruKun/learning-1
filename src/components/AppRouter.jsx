1471857fb043623ffddb2b542d52fe5664ef1cc0
Tabalin Nikolay <Zerru_Kun@yahoo.com>
2022-06-14 15:48:36 +0300

    ПУти в отдельный файл. Добавление страницы Login.

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,22 +1,40 @@
-import React from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
-import About from '../pages/About'
-import Error from '../pages/Error'
-import PostIdPage from '../pages/PostIdPage'
-import Posts from '../pages/Posts'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { privateRoutes, publicRoutes } from "../router/routes";
 
 const AppRouter = () => {
   return (
-    <Routes>
-      <Route path="" element={<Posts />}></Route>
-      <Route path="/about" element={<About />}/>
-      <Route path="/posts" element={<Posts />}/>
-      <Route path="/error" element={<Error />}/>
-      {/* Двоетоечие после id для динамического маршрута */}
-      <Route path="/posts/:id" element={<PostIdPage />} />
-      <Route path="*" element={<Navigate to="/error" />} />
-    </Routes>
-  )
-}
+    // <Routes>
+    //   <Route path="" element={<Posts />}></Route>
+    //   <Route path="/about" element={<About />}/>
+    //   <Route path="/posts" element={<Posts />}/>
+    //   <Route path="/error" element={<Error />}/>
+    //   <Route path="/posts/:id" element={<PostIdPage />} />
+    //   <Route path="*" element={<Navigate to="/error" />} />
+    // </Routes>
+    <div>
+      <Routes>
+        {privateRoutes.map((route) => (
+          <Route
+            key={route.path}
+            path={route.path}
+            element={<route.element />}
+            exact={route.exact}
+          />
+        ))}
+      </Routes>
+      <Routes>
+        {publicRoutes.map((route) => (
+          <Route
+            key={route.path}
+            path={route.path}
+            element={<route.element />}
+            exact={route.exact}
+          />
+        ))}
+      </Routes>
+    </div>
+  );
+};
 
-export default AppRouter
+export default AppRouter;
